387eaeb3ed30d9561e086d86f4c16127d3767d57
kurtbates30 <kurtbts@umich.edu>
2025-10-11 21:03:30 -0400

    finished load csv funtion

--- Project1.py
+++ Project1.py
@@ -1,14 +1,21 @@
-import csv 
+import csv
+import os
+
+print(os.getcwd())
 
 def load_csv(filename):
+    base_path = os.path.dirname(__file__)
+    full_path = os.path.join(base_path, filename)
 
-    with open(filename, 'r', newline='') as file:
+    with open(full_path, 'r', newline='') as file:
         reader = csv.DictReader(file)
         data = [row for row in reader][:50]
     return data
 
 rows = load_csv('SampleSuperstore.csv')
-print(rows)
+#print(rows)
+
+
 
 
 
