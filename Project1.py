59da3eb12de90879e7b53bc20cb3ee1c4512f56e
kurtbates30 <kurtbts@umich.edu>
2025-10-12 20:25:08 -0400

    finished get state data

--- Project1.py
+++ Project1.py
@@ -16,6 +16,18 @@ rows = load_csv('SampleSuperstore.csv')
 #print(rows)
 
 
+def get_state_data(rows):
+    state_data = {}
+    for row in rows:
+        state = row["State"]
+        sales = round(float(row["Sales"]), 1)
+        profit = round(float(row["Profit"]), 1)
+        if state not in state_data:
+            state_data[state] = {"total_sales" : 0, "total_profit": 0}
+        state_data[state]["total_sales"] += sales
+        state_data[state]["total_profit"] += profit
+    return state_data
 
-
+state_data = get_state_data(rows)
+print(state_data)
 
